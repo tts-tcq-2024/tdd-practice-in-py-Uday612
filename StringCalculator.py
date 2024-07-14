a343ee25134216c32cdb96dfa1a3a117d93994f6
Uday612 <69905522+Uday612@users.noreply.github.com>
2024-07-14 23:01:55 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,10 +1,23 @@
-def add(ip_data):
+def process_delimiter(ip_data):
+        delimiter = ip_data[2]
+        number = ip_data.split('\n')
+        numbers_part = ip_data.split('\n')[1]
+        return numbers_part.split(delimiter)
+
+def sum_data(list_data):
     sum = 0
-    
-    if not ip_data:
-        return 0
-    data = ip_data.split(',')
-    for i in data:
+    for i in list_data:
         if i.isdigit() and int(i)<1000:
             sum += int(i)
     return sum
+
+def add(ip_data):
+    
+    if not ip_data:
+        return 0
+        
+    data = ip_data.split(',')
+    if "//" in ip_data:
+        return sum_data(process_delimiter(ip_data))
+        
+    return sum_data(data)
