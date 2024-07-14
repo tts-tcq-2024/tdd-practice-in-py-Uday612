626962717c9ecdd11c2962f9111b5f256f885c32
Uday612 <69905522+Uday612@users.noreply.github.com>
2024-07-14 22:14:29 +0530

    Update StringCalculatorTest.py

--- StringCalculatorTest.py
+++ StringCalculatorTest.py
@@ -9,10 +9,10 @@ class TestStringCalculator(unittest.TestCase):
                 self.assertEqual(add("0"), 0)    
         def test_expectSumForTwoNumberst(self):
                 self.assertEqual(add("1,2"), 3)
-        '''  
+          
         def test_ignoreNumbersGreaterThan1000(self):
                 self.assertEqual(add("1,1001"), 1)
-                
+        '''     
         def test_expectSumWithCustomDelimiter(self):
                 self.assertEqual(add("//;\n1;2"), 3)
                 
